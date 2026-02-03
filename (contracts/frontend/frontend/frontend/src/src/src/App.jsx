266dc40c4f7b32548415dc33eb9cc37a7c216852
Investoremmy <moregold42@gmail.com>
2026-02-03 17:58:41 +0100

    Create App.jsx

---
+++ (contracts/frontend/frontend/frontend/src/src/src/App.jsx
@@ -0,0 +1,51 @@
+import { showConnect } from '@stacks/connect'
+import { userSession } from './wallet'
+import { openContractCall, uintCV } from '@stacks/transactions'
+
+function App() {
+  const connectWallet = () => {
+    showConnect({
+      appDetails: {
+        name: 'Stacks Donation DApp',
+        icon: window.location.origin + '/logo.png'
+      },
+      onFinish: () => {
+        window.location.reload()
+      },
+      userSession
+    })
+  }
+
+  const donate = async () => {
+    const txOptions = {
+      contractAddress: 'ST_CONTRACT_ADDRESS',
+      contractName: 'donation',
+      functionName: 'donate',
+      functionArgs: [uintCV(1)],
+      network: 'mainnet',
+      onFinish: (data) => {
+        alert('Transaction sent: ' + data.txId)
+      }
+    }
+
+    await openContractCall(txOptions)
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Stacks Donation DApp</h1>
+
+      {!userSession.isUserSignedIn() ? (
+        <button onClick={connectWallet}>Connect Wallet</button>
+      ) : (
+        <>
+          <p>Wallet connected</p>
+          <button onClick={donate}>Donate 1 STX</button>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default App
+                            
